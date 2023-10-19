f1ec07b62b414413cdfdbac67f4c376210d00874
kons2003 <mj030313@naver.com>
2023-10-19 15:55:08 +0900

    Dedupe

---
+++ src/Bronze_IV_4/Ex5357.java
@@ -0,0 +1,26 @@
+// Dedupe
+package Bronze_IV_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex5357 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            char[] c = s.toCharArray();
+
+            System.out.print(c[0]);
+            for (int j = 1; j < c.length; j++) {
+                if (c[j - 1] != c[j]) {
+                    System.out.print(c[j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
