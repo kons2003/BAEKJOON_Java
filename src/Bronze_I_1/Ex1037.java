56c74b5472a773a7ee41aad0fe8dc1d38dc103a0
kons2003 <mj030313@naver.com>
2023-11-03 18:49:08 +0900

    약수

---
+++ src/Bronze_I_1/Ex1037.java
@@ -0,0 +1,27 @@
+// 약수
+package Bronze_I_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Ex1037 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        // arr 값 넣는 코드
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr); // arr 오름차순 정렬
+
+        System.out.println(arr[0] * arr[n-1]);
+    }
+}
