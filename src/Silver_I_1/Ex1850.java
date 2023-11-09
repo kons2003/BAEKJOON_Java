752d94352420ed77182a3f827171781c5dc1e344
kons2003 <mj030313@naver.com>
2023-11-09 16:09:08 +0900

    최대공약수(수정중)

--- src/Silver_I_1/Ex1850.java
+++ src/Silver_I_1/Ex1850.java
@@ -1,3 +1,4 @@
+/*
 // 최대공약수
 package Silver_I_1;
 
@@ -23,6 +24,40 @@ public class Ex1850 {
 
         for (int i = 0; i < GCD(a, b); i++) {
             gcd.append(1);
+        }
+
+        System.out.println(gcd);
+    }
+}
+*/
+
+// 최대공약수
+package Silver_I_1;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Ex1850 {
+    static BigInteger GCD(BigInteger a, BigInteger b) {
+        if (b.compareTo(BigInteger.ZERO) == 0) {
+            return a;
+        }
+        return GCD(b, a.remainder(b));
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigInteger gcd = new BigInteger("1");
+
+        BigInteger a = new BigInteger(st.nextToken()); // a의 1 개수
+        BigInteger b = new BigInteger(st.nextToken()); // b의 1 개수
+
+        for (BigInteger i = new BigInteger("1"); i.compareTo(GCD(a, b)) < 0; i.add(BigInteger.ONE)) {
+            gcd = gcd.multiply(BigInteger.TEN);
+            gcd = gcd.add(BigInteger.ONE);
         }
 
         System.out.println(gcd);
