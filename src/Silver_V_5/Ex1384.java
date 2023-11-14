8e53363d9a0cbf7dea2384c272eb595c9bced652
kons2003 <mj030313@naver.com>
2023-11-14 12:16:47 +0900

    메시지(나중에)

---
+++ src/Silver_V_5/Ex1384.java
@@ -0,0 +1,18 @@
+// 메시지
+package Silver_V_5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex1384 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n;
+
+        while ((n = Integer.parseInt(br.readLine())) != 0) {
+            
+        }
+    }
+}
