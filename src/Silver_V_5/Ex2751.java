7803fdfe21916afd02e52d7c18bf364f5da31773
kons2003 <mj030313@naver.com>
2023-12-04 15:15:23 +0900

    수 정렬하기 2

---
+++ src/Silver_V_5/Ex2751.java
@@ -0,0 +1,31 @@
+// 수 정렬하기 2
+package Silver_V_5;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Ex2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] num = new int[n];
+
+        for (int i = 0; i < n; i++) {
+
+            num[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(num);
+
+        for (int i = 0; i < n; i++) {
+
+            bw.write(String.valueOf(num[i]));
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
