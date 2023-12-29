d3adcb6bf256bbb2db4d4e788274acbee82d3b6f
kons2003 <mj030313@naver.com>
2023-12-30 06:15:33 +0900

    분수찾기(나중에)

---
+++ src/Silver_V_5/Ex1193.java
@@ -0,0 +1,17 @@
+// 분수찾기
+package Silver_V_5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex1193 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int x = Integer.parseInt(br.readLine());
+        int width = 0;
+        int length = 0;
+        int cnt = 0;
+    }
+}
