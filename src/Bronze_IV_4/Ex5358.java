4371b0afcd2889530b69fe34785d42aa6b1c29f3
kons2003 <mj030313@naver.com>
2024-01-06 07:09:31 +0900

    Football Team

--- src/Bronze_IV_4/Ex5358.java
+++ src/Bronze_IV_4/Ex5358.java
@@ -6,24 +6,18 @@ import java.io.*;
 public class Ex5358 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String s = "";
         StringBuilder sb = new StringBuilder();
+
+        String s;
         while ((s = br.readLine()) != null && !s.isEmpty()) {
 
             char[] arr = s.toCharArray();
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] == 'e') {
-                    sb.append('i');
-                } else if (arr[i] == 'i') {
-                    sb.append('e');
-                } else if (arr[i] == 'E') {
-                    sb.append('I');
-                } else if (arr[i] == 'I') {
-                    sb.append('E');
-                } else {
-                    sb.append(arr[i]);
-                }
+            for (char c : arr) {
+                if (c == 'e') sb.append('i');
+                else if (c == 'i') sb.append('e');
+                else if (c == 'E') sb.append('I');
+                else if (c == 'I') sb.append('E');
+                else sb.append(c);
             }
             sb.append("\n");
         }
