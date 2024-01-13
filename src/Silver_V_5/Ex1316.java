ff4b04fa5465fdaa75906878d5db504632977be9
kons2003 <mj030313@naver.com>
2024-01-14 08:40:16 +0900

    그룹 단어 체커(나중에)

--- src/Silver_V_5/Ex1316.java
+++ src/Silver_V_5/Ex1316.java
@@ -9,16 +9,31 @@ public class Ex1316 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        char[] words = new char[26];
         boolean[] check = new boolean[26];
         int n = Integer.parseInt(br.readLine());
+        int cnt = n;
+        int before = 26;
 
         for (int i = 0; i < n; i++) {
-            words = br.readLine().toCharArray();
+            char[] words = br.readLine().toCharArray();
+            for (int j = 0; j < check.length; j++) {
+                check[j] = true;
+            }
 
             for (int j = 0; j < words.length; j++) {
-                check[i] = true;
+                int c = (int) words[j] - 97;
+
+                if (check[c]) {
+                    check[c] = false;
+                } else {
+                    cnt--;
+                    break;
+                }
+
+                before = c;
             }
         }
+
+        System.out.println(cnt);
     }
 }
