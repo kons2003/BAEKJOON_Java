5dd503298074ca07c562843e9d8076ad4f575b38
kons2003 <mj030313@naver.com>
2024-01-17 06:37:21 +0900

    그룹 단어 체커

--- src/Silver_V_5/Ex1316.java
+++ src/Silver_V_5/Ex1316.java
@@ -4,33 +4,39 @@ package Silver_V_5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Ex1316 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        boolean[] check = new boolean[26];
+        boolean[] check = new boolean[26]; // 전에 나온 문자인지 검사를 위한 배열
         int n = Integer.parseInt(br.readLine());
-        int cnt = n;
-        int before = 26;
+        int cnt = n; // 그룹 단어의 개수 저장 변수
+        int before = 0; // 바로 전에 나온 문자 저장 변수
 
         for (int i = 0; i < n; i++) {
             char[] words = br.readLine().toCharArray();
-            for (int j = 0; j < check.length; j++) {
-                check[j] = true;
-            }
+            Arrays.fill(check, true);
 
             for (int j = 0; j < words.length; j++) {
                 int c = (int) words[j] - 97;
 
-                if (check[c]) {
+                if (j == 0) {
                     check[c] = false;
-                } else {
-                    cnt--;
-                    break;
+                } else { // j >= 1
+                    if (before != c) {
+                        
+                        if (check[c]) {
+                            check[c] = false;
+                        } else { // check[c] == false
+                            cnt--; // 그룹 단어가 아니면 감소
+                            break;
+                        }
+                    }
                 }
 
-                before = c;
+                before = c; // 이전 문자 저장
             }
         }
 
