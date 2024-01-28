bf9be0485257b91fdeb74444db8c233b74d134d7
kons2003 <mj030313@naver.com>
2024-01-29 06:28:26 +0900

    K번째 수

---
+++ src/Silver_V_5/Ex11004.java
@@ -0,0 +1,30 @@
+// K번째 수
+package Silver_V_5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Ex11004 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+
+        // arr 배열에 값 입력
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr); // 오름차순 정렬
+
+        System.out.println(arr[k - 1]);
+    }
+}
