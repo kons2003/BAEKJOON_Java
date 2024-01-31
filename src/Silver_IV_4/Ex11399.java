5f136fc25ae288b2c34c8ee5e95dce721925ebb0
kons2003 <mj030313@naver.com>
2024-02-01 08:51:13 +0900

    ATM

---
+++ src/Silver_IV_4/Ex11399.java
@@ -0,0 +1,33 @@
+// ATM
+package Silver_IV_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Ex11399 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int sum = 0;
+        int min = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            min += sum;
+        }
+
+        System.out.println(min);
+    }
+}
