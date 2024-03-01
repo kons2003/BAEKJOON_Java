06e8a1582ba7c5a59f2a33c4d805eeaade93971b
kons2003 <mj030313@naver.com>
2024-03-02 06:45:21 +0900

    샤틀버스

---
+++ src/Bronze_IV_4/Ex25625.java
@@ -0,0 +1,23 @@
+// 샤틀버스
+package Bronze_IV_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex25625 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        if (x > y) {
+            System.out.println(x + y);
+        } else {
+            System.out.println(y - x);
+        }
+    }
+}
