04d7e2b313c497b685b1a1b6a789d39191982c07
kons2003 <mj030313@naver.com>
2024-03-05 23:29:13 +0900

    푸앙이와 종윤이

---
+++ src/Bronze_IV_4/Ex25591.java
@@ -0,0 +1,28 @@
+// 푸앙이와 종윤이
+package Bronze_IV_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex25591 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int a = 100 - n;
+        int b = 100 - m;
+        int c = 100 - (a + b);
+        int d = a * b;
+
+        int q = d / 100;
+        int r = d % 100;
+
+        System.out.printf("%d %d %d %d %d %d\n", a, b, c, d, q, r);
+        System.out.printf("%d %d\n", c+q, r);
+    }
+}
