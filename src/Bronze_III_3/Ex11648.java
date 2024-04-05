ffd441769a8b1a1e9386b6e9740ac9975a591180
kons2003 <mj030313@naver.com>
2024-04-05 10:05:24 +0900

    지속

---
+++ src/Bronze_III_3/Ex11648.java
@@ -0,0 +1,26 @@
+// 지속
+package Bronze_III_3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex11648 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+
+        while (n >= 10) {
+            int mul = 1;
+
+            while (n > 0) {
+                mul *= n % 10;
+                n /= 10;
+            }
+            n = mul;
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
