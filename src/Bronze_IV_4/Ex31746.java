61b7e8e6ac695d66d89c3cccfb817d247afd66ae
kons2003 <mj030313@naver.com>
2024-04-07 19:10:03 +0900

    SciComLove (2024)

---
+++ src/Bronze_IV_4/Ex31746.java
@@ -0,0 +1,25 @@
+// SciComLove (2024)
+package Bronze_IV_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex31746 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        char[] str = "SciComLove".toCharArray();
+        char[] rStr = new char[str.length];
+        for (int i = 0; i < str.length; i++) {
+            rStr[str.length - 1 - i] = str[i];
+        }
+
+        if (n % 2 == 0) {
+            System.out.println(str);
+        } else {
+            System.out.println(rStr);
+        }
+    }
+}
