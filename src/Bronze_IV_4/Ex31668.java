d64a3baee0e01459b9e9969a65dad0c8a78ae6d7
kons2003 <mj030313@naver.com>
2024-04-12 09:20:37 +0900

    특별한 가지

---
+++ src/Bronze_IV_4/Ex31668.java
@@ -0,0 +1,17 @@
+// 특별한 가지
+package Bronze_IV_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex31668 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
+
+        System.out.println(m / n * k);
+    }
+}
