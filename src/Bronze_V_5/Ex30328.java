7f09a2bafc09d5fff15f18fe171dfd27d2198a10
kons2003 <mj030313@naver.com>
2024-04-15 09:15:47 +0900

    Java Warriors

---
+++ src/Bronze_V_5/Ex30328.java
@@ -0,0 +1,14 @@
+// Java Warriors
+package Bronze_V_5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex30328 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n * 4000);
+    }
+}
