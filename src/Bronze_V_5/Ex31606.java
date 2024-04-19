2de269f87f7ccbf0cb9883de670da158a5a7ef33
kons2003 <mj030313@naver.com>
2024-04-20 06:57:53 +0900

    果物 (Fruit)

---
+++ src/Bronze_V_5/Ex31606.java
@@ -0,0 +1,16 @@
+// 果物 (Fruit)
+package Bronze_V_5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex31606 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+
+        System.out.println(x + y + 3);
+    }
+}
