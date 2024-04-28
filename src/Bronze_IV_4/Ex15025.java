5bb0c92bcad01ce64237b30f7d3fb57f719ae9cf
kons2003 <mj030313@naver.com>
2024-04-28 16:03:11 +0900

    Judging Moose

---
+++ src/Bronze_IV_4/Ex15025.java
@@ -0,0 +1,24 @@
+// Judging Moose
+package Bronze_IV_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex15025 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int l = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        if (l == 0 && r == 0) {
+            System.out.println("Not a moose");
+        } else if (l == r) {
+            System.out.println("Even " + (l + r));
+        } else {
+            System.out.println("Odd " + Math.max(l, r)*2);
+        }
+    }
+}
