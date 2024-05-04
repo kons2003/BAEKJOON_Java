80dee4e5f7c65f236b9f32a43fa0ab8aa03dfdd4
kons2003 <mj030313@naver.com>
2024-05-04 22:23:15 +0900

    3 つの整数 (Three Integers)

---
+++ src/Bronze_IV_4/Ex18408.java
@@ -0,0 +1,23 @@
+// 3 つの整数 (Three Integers)
+package Bronze_IV_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex18408 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        if (a == b || a == c) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+    }
+}
