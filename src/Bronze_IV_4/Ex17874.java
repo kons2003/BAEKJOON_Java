9044c58022e7c4d889c8c99cfc0b41d3106b4105
kons2003 <mj030313@naver.com>
2024-05-18 22:22:21 +0900

    Piece of Cake!

---
+++ src/Bronze_IV_4/Ex17874.java
@@ -0,0 +1,21 @@
+// Piece of Cake!
+package Bronze_IV_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex17874 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        int res = Math.max(h, n - h) * Math.max(v, n - v);
+
+        System.out.println(res * 4);
+    }
+}
