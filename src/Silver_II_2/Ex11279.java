c3feafda55b73e5c03cc4830626da95902994b12
kons2003 <mj030313@naver.com>
2024-05-23 15:08:52 +0900

    최대 힙

---
+++ src/Silver_II_2/Ex11279.java
@@ -0,0 +1,31 @@
+// 최대 힙
+package Silver_II_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Ex11279 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x == 0) {
+                if (maxHeap.isEmpty()) {
+                    maxHeap.add(0);
+                }
+                System.out.println(maxHeap.poll());
+            } else {
+                maxHeap.add(x);
+            }
+        }
+    }
+}
