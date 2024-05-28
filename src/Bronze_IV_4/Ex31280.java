686b8770ff141574b651990bde238ae49361a1a8
kons2003 <mj030313@naver.com>
2024-05-28 09:04:34 +0900

    ФАКИР

---
+++ src/Bronze_IV_4/Ex31280.java
@@ -0,0 +1,24 @@
+// ФАКИР
+package Bronze_IV_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex31280 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        int result = a + b + c + d;
+        result -= Math.min(a, Math.min(b, Math.min(c, d)));
+
+        System.out.println(result + 1);
+    }
+}
