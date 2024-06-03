83a69b06a5402a6fa116a8f61bbacc329757718d
kons2003 <mj030313@naver.com>
2024-06-03 09:13:51 +0900

    Is Y a Vowel?

---
+++ src/Bronze_IV_4/Ex31306.java
@@ -0,0 +1,28 @@
+// Is Y a Vowel?
+package Bronze_IV_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex31306 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] s = br.readLine().toCharArray();
+
+        int vowelCount = 0;
+        int yCount = 0;
+
+        for (int i = 0; i < s.length; i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+                vowelCount++;
+            }
+            if (s[i] == 'y') {
+                yCount++;
+            }
+        }
+
+        System.out.println(vowelCount +" "+ (vowelCount+yCount));
+    }
+}
