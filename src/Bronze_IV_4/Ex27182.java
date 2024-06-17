93ace236c0ce1120e3d0cf5f993b7afff4f47810
kons2003 <mj030313@naver.com>
2024-06-17 20:24:42 +0900

    Rain Diary

---
+++ src/Bronze_IV_4/Ex27182.java
@@ -0,0 +1,23 @@
+// Rain Diary
+package Bronze_IV_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex27182 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        if (n > 7) {
+            System.out.println(n - 7);
+        } else {
+            System.out.println(m + 7);
+        }
+    }
+}
