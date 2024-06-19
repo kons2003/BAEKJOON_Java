176166c0ba7b96122ef4cc38b526dfcb4a66f74d
kons2003 <mj030313@naver.com>
2024-06-19 10:21:32 +0900

    Poziome serca

--- src/Bronze_IV_4/Ex26772.java
+++ src/Bronze_IV_4/Ex26772.java
@@ -20,14 +20,13 @@ public class Ex26772 {
         };
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < n; j++) {
                 sb.append(heart[i]);
-                if (j != n - 1) { sb.append(" "); }
+                if (j != n - 1) { sb.append(" "); } // n번째 전일때
             }
             sb.append("\n");
         }
