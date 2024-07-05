0a546671c01df0b8ad593f07444c8978ec41e420
kons2003 <mj030313@naver.com>
2024-07-06 00:00:18 +0900

    Circus

---
+++ src/Bronze_IV_4/Ex20352.java
@@ -0,0 +1,11 @@
+// Circus
+package Bronze_IV_4;
+
+import java.util.Scanner;
+
+public class Ex20352 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(2*Math.pow(sc.nextDouble()*Math.PI, 0.5));
+    }
+}
