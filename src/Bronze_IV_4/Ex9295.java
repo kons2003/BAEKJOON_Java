2180d7f8e191e8c435f56c1cb41fe47e352fc7b4
kons2003 <mj030313@naver.com>
2024-07-06 22:31:21 +0900

    주사위

---
+++ src/Bronze_IV_4/Ex9295.java
@@ -0,0 +1,22 @@
+// 주사위
+package Bronze_IV_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex9295 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            System.out.println("Case " + i + ": " + (n + m));
+        }
+    }
+}
